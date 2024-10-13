7016922e2c27adad0c12d8c465cf0cf7653fb833
Shreyas Kothari <spkothari0@gmail.com>
2024-10-12 22:30:44 -0400

    ading security group and ec2

---
+++ terraform/securityGroup.tf
@@ -0,0 +1,55 @@
+# Define the security group for the web application
+resource "aws_security_group" "shreyas_terraform_sg" {
+  name        = "csye6225_app_security_group"
+  description = "Security group for web application"
+  vpc_id      = aws_vpc.shreyas_terraform_vpc.id
+
+  # Ingress rule for SSH (port 22)
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Ingress rule for HTTP (port 80)
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Ingress rule for HTTPS (port 443)
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Ingress rule for the application port (8080)
+  ingress {
+    description = "Allow Application Traffic"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rule for all traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "Application Security Group",
+    Description = "Security group for the web application in CSYE 6225 course Spring Boot Application"
+  }
+}
