6d50220f050025aaaa9014585e5e77bb2268b9f4
Shreyas Kothari <spkothari0@gmail.com>
2024-10-19 21:29:13 -0400

    Assignment 5 changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -38,3 +38,39 @@ variable "sg_cidr" {
   type        = string
   default     = "0.0.0.0/0"
 }
+
+variable "db_engine" {
+  description = "The engine for the RDS instance"
+  type        = string
+  default     = "mysql"
+}
+
+variable "db_engine_version" {
+  description = "The engine version for the RDS instance"
+  type        = string
+  default     = "8.0"
+}
+
+variable "db_instance_class" {
+  description = "The instance class for the RDS instance"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_name" {
+  description = "The instance class for the RDS instance"
+  type        = string
+  default     = "csye6225"
+}
+
+variable "db_username" {
+  description = "The username for the RDS instance"
+  type        = string
+  default     = "csye6224"
+}
+
+variable "db_password" {
+  description = "The password for the RDS instance"
+  type        = string
+  default     = "csye6225"
+}
