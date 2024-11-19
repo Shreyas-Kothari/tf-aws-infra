84c5aa2d849cd23296e0c5ba8e6aa61beac1be6a
Shreyas Kothari <spkothari0@gmail.com>
2024-11-18 21:13:26 -0500

    fromatted

--- terraform/securityGroup.tf
+++ terraform/securityGroup.tf
@@ -93,19 +93,19 @@ resource "aws_security_group" "shreyas_terraform_lb_sg" {
 
   # Allow HTTP (port 80) from anywhere
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   # Allow HTTPS (port 443) from anywhere
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
